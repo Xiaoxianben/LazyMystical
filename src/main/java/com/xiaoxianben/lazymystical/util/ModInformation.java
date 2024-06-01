dccaef8c46fa73099ded3cb02a9f5aa9f933efd2
Xiaoxianben <152693889+Xiaoxianben@users.noreply.github.com>
2024-06-01 17:34:40 +0800

    1.1.2 - 版本号更新

--- src/main/java/com/xiaoxianben/lazymystical/util/ModInformation.java
+++ src/main/java/com/xiaoxianben/lazymystical/util/ModInformation.java
@@ -4,7 +4,7 @@ public class ModInformation {
 
     public static final String MOD_ID = "lazymystical";
     public static final String NAME = "Lazy Mystical";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
     public static final String ACCEPTED_VERSION = "[1.12.2]";
     public static final String CLIENT_PROXY_CLASS = "com.xiaoxianben.lazymystical.proxy.ClientProxy";
     public static final String COMMON_PROXY_CLASS = "com.xiaoxianben.lazymystical.proxy.CommonProxy";
